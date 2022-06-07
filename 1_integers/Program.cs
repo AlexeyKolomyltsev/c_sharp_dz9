dc88a0a38df553f32273415313d4a0b0bc8b2994
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-06-07 21:51:09 +0800

    добавил задачу 1

---
+++ 1_integers/Program.cs
@@ -0,0 +1,13 @@
+﻿///Показать натуральные числа от M до N, N и M заданы
+void Posledovatelnost(int m, int n)
+{
+    Console.Write(m + " ");
+    if(m < n) Posledovatelnost(m + 1, n);
+}
+
+Console.Write("Put number M ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Put number N ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Последовательность чисел: ");
+Posledovatelnost(m,n);
