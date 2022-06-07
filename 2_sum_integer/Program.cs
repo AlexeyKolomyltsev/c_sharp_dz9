5c9407e997a0e21ee2c27151525b3eae77478a3d
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-06-07 22:17:10 +0800

    добавил задачу 2

---
+++ 2_sum_integer/Program.cs
@@ -0,0 +1,14 @@
+﻿///Найти сумму элементов от M до N, N и M заданы
+int SumOfMN(int m, int n)
+{   
+    if(m == n) return m;
+    else return m + SumOfMN(m + 1, n);
+}
+
+Console.Write("Put number M ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Put number N ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write($"Сумма чисел от {m} до {n}: " );
+int sum = SumOfMN(m,n);
+Console.WriteLine(sum);
